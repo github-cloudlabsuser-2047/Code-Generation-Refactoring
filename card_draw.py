70ce5953efe2f032650da1510531ee8173dba7dd
github-cloudlabsuser-2047 <169064633+github-cloudlabsuser-2047@users.noreply.github.com>
2024-11-20 14:17:58 +0000

    card draw debuggin

--- card_draw.py
+++ card_draw.py
@@ -1,15 +1,13 @@
-# Intentionally flawed Python program
-
 # importing modules
-import itertools, random
+import itertools
+import random
 
 # make a deck of cards
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club'])
+deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
 
 # shuffle the cards
 random.shuffle(deck)
 
-# draw five cards
-print("You got:")
-for i in range(5)
-   print(deck[i][0], "of", deck[i][1]
+# draw a card
+drawn_card = deck.pop()
+print(f"Drawn card: {drawn_card[0]} of {drawn_card[1]}")
